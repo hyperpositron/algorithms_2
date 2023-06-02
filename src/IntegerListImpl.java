60a4f59231a81c34377ffd836c876702eaec2114
Александр <hyperpositron@gmail.com>
2023-06-02 17:22:48 +0200

    done

--- src/IntegerListImpl.java
+++ src/IntegerListImpl.java
@@ -69,7 +69,9 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public boolean contains(Integer item) {
-        return indexOf(item) != -1;
+        Integer[] storageCopy = toArray();
+        sort (storageCopy);
+        return binarySearch(storageCopy, item);
     }
 
     @Override
@@ -141,4 +143,35 @@ public class IntegerListImpl implements IntegerList {
         }
     }
 
+    private void sort(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    private boolean binarySearch(Integer [] arr, Integer item) {
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (item == arr[mid]) {
+                return true;
+            }
+
+            if (item < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
+}
 }
